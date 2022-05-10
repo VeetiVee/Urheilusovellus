fea859b5b8a1f8670ca5bada46a9e4015f9007e8
VeetiVee <veeti.vainikka@gmail.com>
2022-05-10 16:18:27 +0300

    added reps to muscle training, doesnt work

--- App.js
+++ App.js
@@ -99,17 +99,20 @@ function Running() {
 function NewTraining() {
   const [move, setMove] = useState(''); 
   const [weight, setWeight] = useState('');
+  const [reps, setReps] = useState('');
   const [pastTraining, setPastTraining] = useState([]);
+
+  // TALLENNUS EI TOIMINUT KUN LISÄSIN "REPS". AJATTELIN ONKO VIKA SQL LAUSEISSA VAI FLAT LISTISSÄ
 
   useEffect(() => {
     db.transaction(tx => {
-      tx.executeSql('create table if not exists pastTraining (id integer primary key not null, move text, weight text);');
+      tx.executeSql('create table if not exists pastTraining (id integer primary key not null, move text, weight text, reps text);');
     }, null, updateList); 
   }, []);
 
   const saveTraining = () => {
     db.transaction(tx => {
-        tx.executeSql('insert into pastTraining (move, weight) values (?, ?);', [move, weight]);    
+        tx.executeSql('insert into pastTraining (move, weight, reps) values (?, ?, ?);', [move, weight, reps]);    
       }, null, updateList
     )
   }
@@ -143,6 +146,8 @@ function NewTraining() {
     );
   };
 
+  // VIRHE VOISI MYÖS OLLA FLATLISTIN RENDER ITEMISSA TAI SITTEN VOI OLLA JOKIN TOINENKIN VIRHE
+
   return (
     <View style={styles.container}>
       <TextInput placeholder='Move' style={{marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
@@ -150,13 +155,16 @@ function NewTraining() {
         value={move}/>  
       <TextInput placeholder='Weight' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
         onChangeText={(weight) => setWeight(weight)}
-        value={weight}/>      
+        value={weight}/> 
+      <TextInput placeholder='Reps' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(reps) => setReps(reps)}
+        value={reps}/>           
       <Button onPress={saveTraining} title="Save" /> 
       <Text style={{marginTop: 30, fontSize: 20}}>Past trainings</Text>
       <FlatList 
         style={{marginLeft : "5%"}}
         keyExtractor={item => item.id.toString()} 
-        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.move}, {item.weight}</Text>
+        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.move}, {item.weight}, {item.reps}</Text>
         <Text style={{fontSize: 18, color: '#0000ff'}} onPress={() => deleteTraining(item.id)}> Delete</Text></View>} 
         data={pastTraining} 
         ItemSeparatorComponent={listSeparator} 
